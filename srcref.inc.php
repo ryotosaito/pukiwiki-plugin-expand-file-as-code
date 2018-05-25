4c376ffc55907c8b9acf73e25bf89aa24362925e
Ryoto Saito <ryoto.no.address@gmail.com>
2018-05-25 09:58:47 +0900

    [Add] plugin format

---
+++ srcref.inc.php
@@ -0,0 +1,14 @@
+<?php
+// PukiWiki - Yet another WikiWikiWeb clone
+// // $Id: srcref.inc.php $
+// // Copyright (C)
+// //   2018 Ryoto Saito
+// // License: MIT
+// //
+// // Source code reference plugin
+// // Include an attached source-file as a link and a preformatted-text
+//
+function plugin_srcref_convert()
+{
+	return "";
+}
